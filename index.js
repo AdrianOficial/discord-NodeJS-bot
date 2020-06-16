790df602e85b90f8f99a95f456e1552927423efc
AdrianOficial <numai_jocuri23@yahoo.com>
2020-06-16 10:48:49 +0300

    Comenzile mutate pe switch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Discord = require('discord.js');
-const botToken = "NzE2MTY2NTQ4NDE0NTI5NTU3.XuTmKg.ei0TEP5q4bK-18HzQEa8VLWi3cA";
+const botToken = "TOKEN";
 const prefix = "!";
 const { MessageAttachment } = require('discord.js');
 const activity = "invat NodeJS";
@@ -51,222 +51,222 @@ client.on('message', message => {
 		if (x) console.error(x)
 	});
 
-	if(command === 'restart') {
-		if(admin.includes(message.author["id"])) {
-			message.channel.send('Resetting...').then(msg => client.destroy()).then(() => client.login(botToken)).then(client.user.setActivity(activity));
-		}else{
-			message.channel.send(`N-ai admin`);
-		}
-	}
+	switch(command) {
+		case 'restart': {
+			if(admin.includes(message.author["id"])) {
+				message.channel.send('Resetting...').then(msg => client.destroy()).then(() => client.login(botToken)).then(client.user.setActivity(activity));
+			}else{
+				message.channel.send(`N-ai admin`);
+			};
+			break;
+		};
+		case 'strawpoll': {
+			if (message.member.hasPermission('ADMINISTRATOR')) {
+				var strawpoll = message.content.slice(prefix.length).split("strawpoll")[1];
 
+				const Embed = new Discord.MessageEmbed()
+				.setTitle(`ℹ️ ${strawpoll}`)
+				.setColor('#4086bb')
+				.setFooter(`${message.author.username} a creat acest strawpoll`);
 
-	if(command === 'strawpoll') {
-		if (message.member.hasPermission('ADMINISTRATOR')) {
-			var strawpoll = message.content.slice(prefix.length).split("strawpoll")[1];
-
-			const Embed = new Discord.MessageEmbed()
-			.setTitle(`ℹ️ ${strawpoll}`)
-			.setColor('#4086bb')
-			.setFooter(`${message.author.username} a creat acest strawpoll`);
-
-			message.delete().then(async () => {
-				await message.channel.send(Embed).then(async message=> {
-					await message.react('👍');
-					await message.react('👎');
+				message.delete().then(async () => {
+					await message.channel.send(Embed).then(async message=> {
+						await message.react('👍');
+						await message.react('👎');
+					});
 				});
-			});
-		}
+			};
+			break;
+		};
+		case 'purge': {
+			if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu ai acces la această comandă");
+			if (!args[0]) return message.reply("Folosește: !purge <număr>");
 
-	}
-
-	if(command === 'purge') {
-		if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu ai acces la această comandă");
-		if (!args[0]) return message.reply("Folosește: !purge <număr>");
-
-		if (Number.isInteger(parseInt(args[0]))) {
-			var amount = parseInt(args[0]);
-			if(amount > 0) {
-				message.channel.bulkDelete(amount).then(() => {
-					if (args[0] === '1') {
-						message.channel.send(`Au fost șterse ${args[0]} mesaje`).then(msg => msg.delete({ 
-							timeout: 3000 
-						}));
-					} else {
-						message.channel.send(`Au fost șterse ${args[0]} mesaje`).then(msg => msg.delete({
-							timeout: 3000
-						}));
-					};
-				});
-			}
-		}else{
-			console.log('Nu e număr');
-		}
-	}
-
-	if(command === 'ping') {
-		message.channel.send(`**PONG! 🏓 Latenţă: ${client.ws.ping}ms**`);
-	}
-
-	if (command === 'infobot') {
-		let Embed = new Discord.MessageEmbed()
-		.setTitle("Informații BOT")
-		.setThumbnail(client.user.displayAvatarURL())
-		.addField("Nume bot", client.user.username)
-		.addField("Creat pe data de", client.user.createdAt)
-		.addField("Creat folosind", "JavaScript, NodeJS, discord.js", true)
-		.addField("Creat de", "Adryan#0870", true)
-		.addField("Versiunea botului", versiune, true)
-		.setTimestamp()
-		.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
-		
-		message.channel.send(Embed);
-		
-	}
-
-	if(command === 'duma') {
-		fs.readFile('dume.json', (err, data) => {
-		    if (err) throw err;
-		    let datas = JSON.parse(data);
-		    const count = datas.dume.push();
-		    const randomDuma = Math.floor(Math.random() * Math.floor(count));
-		    if(args[0]) {
-		    	const name = message.mentions.users.first();
-		    	message.channel.send(`${name} ${datas.dume[randomDuma]}`);
-		    }else{
-		    	message.channel.send(`${message.author} ${datas.dume[randomDuma]}`);
-		    }
-		});
-	}
-
-	if(command === 'cat') {
-		let client = http;
-		var url = 'http://api.thecatapi.com/v1/images/search';
-
-		client.get(url, (resp) => {
-			let data = '';
-			resp.on('data', (chunk) => {
-				data += chunk;
-			});
-			resp.on('end', () => {
-				var object = JSON.parse(data);
-				const attachment = new MessageAttachment(object[0].url);
-				message.channel.send(attachment);
-			});
-
-		});
-	}
-
-	if(command === 'dog') {
-		let client = https;
-		var url = 'https://api.thedogapi.com/v1/images/search';
-
-		client.get(url, (resp) => {
-			let data = '';
-			resp.on('data', (chunk) => {
-				data += chunk;
-			});
-			resp.on('end', () => {
-				var object = JSON.parse(data);
-				const attachment = new MessageAttachment(object[0].url);
-				message.channel.send(attachment);
-			});
-
-		});
-	}
-
-	if(command === 'adduma') {
-		if(admin.includes(message.author["id"])) {
-			var duma = message.content.slice(prefix.length).split("adduma")[1];
-			fs.readFile('dume.json', 'utf8', function (err, data) {
-				if (err) {
-					console.log(err)
-				}else{
-					const file = JSON.parse(data);
-					file.dume.push(duma);
-					const json = JSON.stringify(file);
-					fs.writeFile('dume.json', json, 'utf8', function(err){
-						if(err){ 
-							console.log(err);
-						}else{
-							message.channel.send(`Duma a fost adăugată cu succes`);
-						}
+			if (Number.isInteger(parseInt(args[0]))) {
+				var amount = parseInt(args[0]);
+				if(amount > 0) {
+					message.channel.bulkDelete(amount).then(() => {
+						if (args[0] === '1') {
+							message.channel.send(`Au fost șterse ${args[0]} mesaje`).then(msg => msg.delete({ 
+								timeout: 3000 
+							}));
+						} else {
+							message.channel.send(`Au fost șterse ${args[0]} mesaje`).then(msg => msg.delete({
+								timeout: 3000
+							}));
+						};
 					});
 				}
+			}else{
+				console.log('Nu e număr');
+			};
+			break;
+		};
+		case 'ping': {
+			message.channel.send(`**PONG! 🏓 Latenţă: ${client.ws.ping}ms**`);
+			break;
+		};
+		case 'infobot': {
+			let Embed = new Discord.MessageEmbed()
+			.setTitle("Informații BOT")
+			.setThumbnail(client.user.displayAvatarURL())
+			.addField("Nume bot", client.user.username)
+			.addField("Creat pe data de", client.user.createdAt)
+			.addField("Creat folosind", "JavaScript, NodeJS, discord.js", true)
+			.addField("Creat de", "Adryan#0870", true)
+			.addField("Versiunea botului", versiune, true)
+			.setTimestamp()
+			.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
+			
+			message.channel.send(Embed);
+			break;
+		};
+		case 'duma': {
+			fs.readFile('dume.json', (err, data) => {
+			    if (err) throw err;
+			    let datas = JSON.parse(data);
+			    const count = datas.dume.push();
+			    const randomDuma = Math.floor(Math.random() * Math.floor(count));
+			    if(args[0]) {
+			    	const name = message.mentions.users.first();
+			    	message.channel.send(`${name} ${datas.dume[randomDuma]}`);
+			    }else{
+			    	message.channel.send(`${message.author} ${datas.dume[randomDuma]}`);
+			    }
 			});
-		}
-	}
+			break;
+		};
+		case 'cat': {
+			let client = http;
+			var url = 'http://api.thecatapi.com/v1/images/search';
 
-	if (command === 'prost') {
-		if(args[0]) {
-			const name = message.mentions.users.first();
-			message.channel.send(`${name} este ${Math.floor(Math.random() * Math.floor(100))}% prost`);
-		}else{
-			message.channel.send(`${message.author} esti ${Math.floor(Math.random() * Math.floor(100))}% prost`);
-		}
-	}
-
-	if (command === 'avatar') {
-		if(args[0]) {
-			const name = message.mentions.users.first();
-			client.users.fetch(name['id']).then(utilizator => {
-			    const attachment = new MessageAttachment(utilizator.avatarURL());
-			    message.channel.send(attachment);
-			});
-		}else{
-			const attachment = new MessageAttachment(message.author.displayAvatarURL());
-			message.channel.send(attachment);
-		}
-	};
-
-
-	if (command === 'idle') {
-		if(admin.includes(message.author["id"])) {
-			client.user.setStatus('idle');
-			console.log(`${message.member} a folosit comanda !idle`);
-			fs.appendFileSync('logs.txt', `${message.member} a folosit comanda !idle
-`, function (err) {
-				if (err) return console.log(err);
-				console.log(`${message.member} a încercat să folosească comanda !idle > logs.txt `);
-			});
-		}else{
-			message.channel.send(`${message.member} nu ai functia de Administrator`);
-			console.log(`${message.member} a încercat să folosească comanda !idle`);
-			fs.appendFileSync('logs.txt', `${message.member} a încercat să folosească comanda !idle
-`, function (err) {
-				if (err) return console.log(err);
-				console.log(`${message.member} a încercat să folosească comanda !idle > logs.txt `);
-			});
-		}
-	};
-
-
-	if (command === 'ban') {
-		if(args[0]) {
-			var membru = message.mentions.members.first();
-			if(message.member.hasPermission('BAN_MEMBERS')) {
-				membru.ban().then((member) => {
-					message.channel.send(`:wave: ${member.displayName} a fost banat cu succes de către :point_right: ${message.member}`);
+			client.get(url, (resp) => {
+				let data = '';
+				resp.on('data', (chunk) => {
+					data += chunk;
 				});
-			}
-		}else{
-			message.channel.send(`Folosește comanda !ban {nume} pentru a bana un membru.`);
+				resp.on('end', () => {
+					var object = JSON.parse(data);
+					const attachment = new MessageAttachment(object[0].url);
+					message.channel.send(attachment);
+				});
+
+			});
+			break;
+		};
+		case 'dog': {
+			let client = https;
+			var url = 'https://api.thedogapi.com/v1/images/search';
+
+			client.get(url, (resp) => {
+				let data = '';
+				resp.on('data', (chunk) => {
+					data += chunk;
+				});
+				resp.on('end', () => {
+					var object = JSON.parse(data);
+					const attachment = new MessageAttachment(object[0].url);
+					message.channel.send(attachment);
+				});
+
+			});
+			break;
+		};
+		case 'adduma': {
+			if(admin.includes(message.author["id"])) {
+				var duma = message.content.slice(prefix.length).split("adduma")[1];
+				fs.readFile('dume.json', 'utf8', function (err, data) {
+					if (err) {
+						console.log(err)
+					}else{
+						const file = JSON.parse(data);
+						file.dume.push(duma);
+						const json = JSON.stringify(file);
+						fs.writeFile('dume.json', json, 'utf8', function(err){
+							if(err){ 
+								console.log(err);
+							}else{
+								message.channel.send(`Duma a fost adăugată cu succes`);
+							}
+						});
+					}
+				});
+			};
+			break;
+		};
+		case 'prost': {
+			if(args[0]) {
+				const name = message.mentions.users.first();
+				message.channel.send(`${name} este ${Math.floor(Math.random() * Math.floor(100))}% prost`);
+			}else{
+				message.channel.send(`${message.author} esti ${Math.floor(Math.random() * Math.floor(100))}% prost`);
+			};
+			break;
+		};
+		case 'avatar': {
+			if(args[0]) {
+				const name = message.mentions.users.first();
+				client.users.fetch(name['id']).then(utilizator => {
+				    const attachment = new MessageAttachment(utilizator.avatarURL());
+				    message.channel.send(attachment);
+				});
+			}else{
+				const attachment = new MessageAttachment(message.author.displayAvatarURL());
+				message.channel.send(attachment);
+			};
+			break;
+		};
+		case 'idle': {
+			if(admin.includes(message.author["id"])) {
+				client.user.setStatus('idle');
+				console.log(`${message.member} a folosit comanda !idle`);
+				fs.appendFileSync('logs.txt', `${message.member} a folosit comanda !idle
+	`, function (err) {
+					if (err) return console.log(err);
+					console.log(`${message.member} a încercat să folosească comanda !idle > logs.txt `);
+				});
+			}else{
+				message.channel.send(`${message.member} nu ai functia de Administrator`);
+				console.log(`${message.member} a încercat să folosească comanda !idle`);
+				fs.appendFileSync('logs.txt', `${message.member} a încercat să folosească comanda !idle
+	`, function (err) {
+					if (err) return console.log(err);
+					console.log(`${message.member} a încercat să folosească comanda !idle > logs.txt `);
+				});
+			};
+			break;
+		};
+
+		case 'ban': {
+			if(args[0]) {
+				var membru = message.mentions.members.first();
+				if(message.member.hasPermission('BAN_MEMBERS')) {
+					membru.ban().then((member) => {
+						message.channel.send(`:wave: ${member.displayName} a fost banat cu succes de către :point_right: ${message.member}`);
+					});
+				}
+			}else{
+				message.channel.send(`Folosește comanda !ban {nume} pentru a bana un membru.`);
+			};
+			break;
+		};
+
+		case 'kick': {
+			if(args[0]) {
+				var membru = message.mentions.members.first();
+				if(message.member.hasPermission('KICK_MEMBERS')) {
+					membru.kick().then((member) => {
+						message.channel.send(`:wave: ${member.displayName} a primit kick de la :point_right: ${message.member}`);
+					});
+				}
+			}else{
+				message.channel.send(`Folosește comanda !kick {nume} pentru a putea da kick unui membru.`);
+			};
+			break;
 		};
 
 	};
 
-	if (command === 'kick') {
-		if(args[0]) {
-			var membru = message.mentions.members.first();
-			if(message.member.hasPermission('KICK_MEMBERS')) {
-				membru.kick().then((member) => {
-					message.channel.send(`:wave: ${member.displayName} a primit kick de la :point_right: ${message.member}`);
-				});
-			}
-		}else{
-			message.channel.send(`Folosește comanda !kick {nume} pentru a putea da kick unui membru.`);
-		};
-	};
-	
 });
 
 client.on('guildMemberAdd', member => {
